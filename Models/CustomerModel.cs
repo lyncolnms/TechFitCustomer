828f207701bc28c74b35b528eb78fff40c48f1e5
lyncolnmauricio <lyncolnms@gmail.com>
2025-08-30 20:32:42 -0300

    add model

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TechFitCustomer.Models;
+
+public class CustomerModel
+{
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
