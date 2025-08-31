b43e466bf1d07ced4ccad14d9bd4fa93df43a4cc
Lyncoln Mauricio <lyncolnms@gmail.com>
2025-08-31 01:26:11 -0300

    add CommunityToolkit integration and update project dependencies

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using TechFitCustomer.Features.Main;
 
 namespace TechFitCustomer;
 
@@ -6,14 +8,17 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        var builder = MauiApp.CreateBuilder();
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        builder.Services.AddTransient<MainPage, MainPageViewModel>();
 
 #if DEBUG
         builder.Logging.AddDebug();
