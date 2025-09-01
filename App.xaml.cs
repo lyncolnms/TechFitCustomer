cb50ff5d301f03c4c2825284a3eddef984bfd3ea
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-01 02:53:42 -0300

    add window management for Customer Manager on Windows platform

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace TechFitCustomer;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace TechFitCustomer;
 
 public partial class App : Application
 {
@@ -9,6 +15,37 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        Window window = new Window(new AppShell())
+        {
+            Title = "Customer Manager"
+        };
+
+#if WINDOWS
+        window.Created += (_, _) =>
+        {
+            if (window.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow)
+            {
+                IntPtr handle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Win32Interop.GetWindowIdFromWindow(handle);
+                AppWindow? appWindow = AppWindow.GetFromWindowId(windowId);
+                
+                if (appWindow != null)
+                {
+                    DisplayArea? displayArea = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+                    if (displayArea != null)
+                    {
+                        RectInt32 workArea = displayArea.WorkArea;
+                        appWindow.MoveAndResize(new RectInt32(
+                            workArea.X, 
+                            workArea.Y, 
+                            workArea.Width, 
+                            workArea.Height));
+                    }
+                }
+            }
+        };
+#endif
+
+        return window;
     }
 }
