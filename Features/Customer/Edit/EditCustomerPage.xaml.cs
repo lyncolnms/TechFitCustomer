d794822865168dc2151e1d13431e5e45f6bf5295
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-01 02:54:30 -0300

    add CancelCommand to CustomerFormView in EditCustomerPage

--- Features/Customer/Edit/EditCustomerPage.xaml.cs
+++ Features/Customer/Edit/EditCustomerPage.xaml.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TechFitCustomer.Features.Customer.Edit;
 
 public partial class EditCustomerPage : ContentPage
 {
+    public EditCustomerPageViewModel ViewModel { get; }
+    
     public EditCustomerPage(EditCustomerPageViewModel viewModel)
     {
         InitializeComponent();
+        ViewModel = viewModel;
         BindingContext = viewModel;
     }
 }
