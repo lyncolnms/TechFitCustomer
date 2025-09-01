b9cbb36969c978d85baf62c93b1f7e5219f2ab7d
lyncolnmauricio <lyncolnms@gmail.com>
2025-09-01 02:48:02 -0300

    add Id property and initialize string properties in CustomerModel

--- Models/CustomerModel.cs
+++ Models/CustomerModel.cs
@@ -2,8 +2,9 @@
 
 public class CustomerModel
 {
-    public string Name { get; set; }
-    public string Lastname { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
     public int Age { get; set; }
-    public string Address { get; set; }
+    public string Address { get; set; } = string.Empty;
 }
